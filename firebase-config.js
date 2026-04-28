82d4505083aba264c0f1839de643c11768e477e4
heeemo2 <hakeerg111@gmail.com>
2026-04-28 11:00:37 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
-  apiKey: "test",
-  authDomain: "test",
-  databaseURL: "test",
-  projectId: "test",
-  storageBucket: "test",
-  messagingSenderId: "test",
-  appId: "test"
+  apiKey: "AIzaSyCXA1x9fJe6zPFo7yiK1kSRsoR89aSff5k",
+  authDomain: "itchat-web-8c4ed.firebaseapp.com",
+  databaseURL: "https://itchat-web-8c4ed-default-rtdb.firebaseio.com",
+  projectId: "itchat-web-8c4ed",
+  storageBucket: "itchat-web-8c4ed.firebasestorage.app",
+  messagingSenderId: "787261764804",
+  appId: "1:787261764804:web:68cfdba7878669c7dbc591",
+  measurementId: "G-G8S46BLQ0Y"
 };
-
 const ADMIN_UIDS = [
   "37KCQRcgyjYPFZdj14uNJpCiKZk2"
 ];
