6de8cd4a99c62c01b446f83d7a9f2db1cebe9d85
heeemo2 <hakeerg111@gmail.com>
2026-04-28 11:50:04 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -23,7 +23,7 @@ const firebaseConfig = {
 //  وضعه هنا بين علامتي التنصيص
 // ════════════════════════════════════════
 const ADMIN_UIDS = [
-  "37KCQRcgyjYPFZdj14uNJpCiKZk2",
+  "jBvCAZ10dKNxNHJVCidWhi6J7W22",
 ];
 
 // ════════════════════════════════════════
